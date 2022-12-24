ce65dec591d5efad416a2fdcf229295aaf8b6aaf
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2022-12-24 15:30:18 +0700

    Update MainActivity.kt

--- app/src/main/java/com/rivvana/datastore/MainActivity.kt
+++ app/src/main/java/com/rivvana/datastore/MainActivity.kt
@@ -1,18 +1,8 @@
 package com.rivvana.datastore
 
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.core.intPreferencesKey
-import androidx.datastore.preferences.preferencesDataStore
 import com.rivvana.datastore.databinding.ActivityMainBinding
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.map
-import java.util.concurrent.Flow
-import java.util.prefs.Preferences
-import androidx.datastore.preferences.core.Preferences as Preferences1
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -21,7 +11,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        
+
 
 
     }
