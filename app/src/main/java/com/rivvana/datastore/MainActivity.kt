822bb1ae1fb9b5e864dc474dbfca77c96df68f3a
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2022-12-24 15:27:54 +0700

    .

--- app/src/main/java/com/rivvana/datastore/MainActivity.kt
+++ app/src/main/java/com/rivvana/datastore/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.intPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
+import com.rivvana.datastore.databinding.ActivityMainBinding
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
 import java.util.concurrent.Flow
@@ -14,33 +15,14 @@ import java.util.prefs.Preferences
 import androidx.datastore.preferences.core.Preferences as Preferences1
 
 class MainActivity : AppCompatActivity() {
-    // At the top level of your kotlin file:
-    val Context.dataStore: DataStore<Preferences1> by preferencesDataStore(name = "counter")
-    private val COUNTER_KEY = intPreferencesKey("counter_key")
-
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        suspend fun incrementCounter(context: Context){
-            context.dataStore.edit { preferences ->
-                val currentCounterValue = preferences[COUNTER_KEY] ?:0
-                preferences[COUNTER_KEY] = currentCounterValue + 1
-            }
-        }
+        
 
-        suspend fun getPreferences(context: Context): kotlinx.coroutines.flow.Flow<androidx.datastore.preferences.core.Preferences>{
-            return context.dataStore.data
-        }
 
-        suspend fun getCounter(context: Context): kotlinx.coroutines.flow.Flow<Int>{
-            return context.dataStore.data.map { preferences ->
-                preferences[COUNTER_KEY] ?:0
-            }
-        }
-
-        suspend fun getPreferences(context: Context): Flow<Preferences>{
-
-        }
     }
 }
